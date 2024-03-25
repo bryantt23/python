50cecaf21cf91e767251939383b19eee1523de8b
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-25 03:45:01 -0700

    server log challenge

---
+++ server_log.py
@@ -0,0 +1,6 @@
+# Expected Tests to Pass
+# Based on the provided log content, here are some tests your script should aim to pass:
+
+# The most frequently visited URL should be /index.html.
+# The count of 4XX status codes should be 2 (403 and 404).
+# The count of 5XX status codes should be 2 (503 and 500).
