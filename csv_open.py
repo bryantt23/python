e172c0fb5bd23dad709d0fb8661dba484f4f8435
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-03-25 03:00:13 -0700

    csv task
    add sample file

---
+++ csv_open.py
@@ -0,0 +1,12 @@
+# Your script should output something similar to this (based on the provided CSV content):
+
+# Contents of CSV:
+# Name,Age,Score
+# John,23,88
+# Doe,34,92
+# Jane,29,95
+# Smith,40,78
+
+# Average Score: 88.25
+# Highest Scorer: Jane
+# Average Age: 31.5
